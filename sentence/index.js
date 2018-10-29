600f8e0dfa2125b98ccfdf723065e1a8e5925019
yleao <u.yleao@gmail.com>
2018-10-30 00:18:46 +0800

    Site updated: 2018-10-30 00:18:44

--- sentence/index.js
+++
@@ -1,1 +0,0 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>"use strict";$(document).ready(function(){$("#fullpage").fullpage({loopBottom:!0,verticalCentered:!1})});var app=new Vue({el:"#fullpage",data:{}});
