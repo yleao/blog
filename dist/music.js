a103b02c9bccf91792554bcd9d9771b1b849c704
yleao <u.yleao@gmail.com>
2021-05-04 18:51:17 +0800

    Site updated: 2021-05-04 18:51:16

--- dist/music.js
+++ dist/music.js
@@ -9,9 +9,9 @@ const ap = new APlayer({
 	volume: 0.7,
     audio: [
       {
-        name: "Moon River　",
-        artist: 'Audrey Hepburn',
-        url: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/music/moon%20river.mp3',
+        name: "バッハ无伴奏チェロ组曲第一番ト长调作品1007より前奏曲　",
+        artist: ' ',
+        url: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/music/%E3%83%90%E3%83%83%E3%83%8F%E6%97%A0%E4%BC%B4%E5%A5%8F%E3%83%81%E3%82%A7%E3%83%AD%E7%BB%84%E6%9B%B2%20%E7%AC%AC%E4%B8%80%E7%95%AA%E3%83%88%E9%95%BF%E8%B0%83%20%E4%BD%9C%E5%93%811007%E3%82%88%E3%82%8A%E5%89%8D%E5%A5%8F%E6%9B%B2.mp3',
         cover: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/image/音乐图标.jpg',
       },
 	  {
@@ -21,9 +21,9 @@ const ap = new APlayer({
         cover: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/image/音乐图标.jpg',
       },
 	  {
-        name: "one flew over the cuckoo nest",
+        name: "Where Have All The Flowers Gone　",
         artist: ' ',
-        url: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/music/One%20Flew%20over%20the%20Cuckoos%20Nest.mp3',
+        url: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/music/Where%20Have%20All%20The%20Flowers%20Gone.mp3',
         cover: 'https://image-1253170309.cos.ap-guangzhou.myqcloud.com/image/音乐图标.jpg',
       }
     ]
